dfe50b6600f48baa56f61698fe9ae5dd4e7d7006
napple29 <snwxxx29@gmail.com>
2022-03-17 16:26:11 +0900

    TOPページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  root to: 'users#new'
+  devise_for :users, path: ''
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
