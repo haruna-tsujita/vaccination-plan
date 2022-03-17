d9849a15c8baee705b5112fff6b58d11533ef3cd
napple29 <snwxxx29@gmail.com>
2022-03-17 17:36:17 +0900

    rails g devise:i18n:locale ja

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module VaccinationPlan
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.i18n.default_locale = :ja
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
