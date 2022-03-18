4c589c30e7d57f5b09ce22b0947454754a999c8f
napple29 <snwxxx29@gmail.com>
2022-03-18 22:46:17 +0900

    rubocopの指摘を修正

--- test/controllers/children_controller_test.rb
+++ test/controllers/children_controller_test.rb
@@ -1,22 +1,24 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class ChildrenControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get children_index_url
     assert_response :success
   end
 
-  test "should get show" do
+  test 'should get show' do
     get children_show_url
     assert_response :success
   end
 
-  test "should get new" do
+  test 'should get new' do
     get children_new_url
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get children_edit_url
     assert_response :success
   end
