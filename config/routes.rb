2a7d166795906f52dccf74a014181f2ea4cfd3ec
napple29 <snwxxx29@gmail.com>
2022-03-18 12:17:14 +0900

    TOPページのルーティングを整理、命名を変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'users#new'
+  root 'top#index'
   devise_for :users, path: ''
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
