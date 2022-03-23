06d5bd15ad44c1e79d32c26f6518e36e88b5e199
napple29 <snwxxx29@gmail.com>
2022-03-23 13:12:36 +0900

    Selenium::WebDriver::Error::UnknownError: unknown error: Chrome failed to start: crashed.を解消するためにselenium関連の設定を行なった

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -3,5 +3,11 @@
 require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  if ENV['HEADED']
+    driven_by :selenium, using: :chrome
+  else
+    driven_by(:selenium, using: :headless_chrome) do |driver_option|
+      driver_option.add_argument('--no-sandbox')
+    end
+  end
 end
