28d69ed74af5a3c4920fe53470c2ddb00f50e683
napple29 <snwxxx29@gmail.com>
2022-03-23 12:45:29 +0900

    ログイン前後でのページ遷移に関するテストを追加

---
+++ test/system/children_test.rb
@@ -0,0 +1,27 @@
+#frozen_string_literal: true
+
+require 'test_helper'
+require "application_system_test_case"
+
+class Childrentest < ApplicationSystemTestCase
+  include Warden::Test::Helpers
+
+  def setup
+    Warden.test_mode!
+    @user = users( :Alice )
+    login_as(@user, :scope => :user)
+  end
+
+  test 'after_login_visit_children_index' do
+    setup
+    visit children_path
+    assert_text 'Children#index'
+  end
+
+  test 'not_login_do_not_visit_children_index' do
+    setup
+    logout
+    visit children_path
+    assert_text 'ログイン'
+  end
+end
