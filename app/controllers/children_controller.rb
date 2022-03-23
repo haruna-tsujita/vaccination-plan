e0cf2ac3ef3a9ee04f0be0511d5ad9c524737282
napple29 <snwxxx29@gmail.com>
2022-03-23 14:25:02 +0900

    子どもの登録画面の作成

--- app/controllers/children_controller.rb
+++ app/controllers/children_controller.rb
@@ -5,7 +5,30 @@ class ChildrenController < ApplicationController
 
   def show; end
 
-  def new; end
+  def new
+    @child = Child.new
+  end
+
+  def create
+    @child = Child.new(child_params)
+
+    respond_to do |format|
+      if @child.save
+        format.html { redirect_to child_url(@child), notice: 'Child was successfully created.' }
+        format.json { render :show, status: :created, location: @child }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @child.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
   def edit; end
+
+  private
+
+  # Only allow a list of trusted parameters through.
+  def child_params
+    params.require(:child).permit(:name, :birthday)
+  end
 end
