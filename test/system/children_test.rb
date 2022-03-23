2f7b32cade47187d8c09d2f25fcff57bfb6af6d8
napple29 <snwxxx29@gmail.com>
2022-03-23 12:54:19 +0900

    rubocopの指摘を修正

--- test/system/children_test.rb
+++ test/system/children_test.rb
@@ -1,15 +1,15 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
 require 'test_helper'
-require "application_system_test_case"
+require 'application_system_test_case'
 
 class Childrentest < ApplicationSystemTestCase
   include Warden::Test::Helpers
 
   def setup
     Warden.test_mode!
-    @user = users( :Alice )
-    login_as(@user, :scope => :user)
+    @user = users(:Alice)
+    login_as(@user, scope: :user)
   end
 
   test 'after_login_visit_children_index' do
