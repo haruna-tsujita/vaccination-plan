9ff07c18bb41592cb5fa0edc62a868c016f171c1
napple29 <snwxxx29@gmail.com>
2022-03-24 10:00:58 +0900

    バリデーションを追加

--- app/models/child.rb
+++ app/models/child.rb
@@ -2,4 +2,12 @@
 
 class Child < ApplicationRecord
   has_one_attached :avatar
+
+  validates :name, presence: true
+  validates :birthday, presence: true
+  validate :birthday_before_today
+
+  def birthday_before_today
+    errors.add(:birthday, 'は今日以前の日付にしてください') if birthday > Date.current
+  end
 end
