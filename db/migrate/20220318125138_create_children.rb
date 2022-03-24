5004c32dd4a009394ef5ea67b3a051611e53155d
napple29 <snwxxx29@gmail.com>
2022-03-24 10:39:02 +0900

    DB側にNOT NULL制約をつけた

--- db/migrate/20220318125138_create_children.rb
+++ db/migrate/20220318125138_create_children.rb
@@ -1,8 +1,8 @@
 class CreateChildren < ActiveRecord::Migration[6.1]
   def change
     create_table :children do |t|
-      t.string :name
-      t.date :birthday
+      t.string :name, null:false
+      t.date :birthday, null:false
       t.references :user, foreign_key: true
       t.timestamps
     end
