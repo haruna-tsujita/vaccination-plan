a022f73e9c608e7b1dd1bd608bef983b307081d4
napple29 <snwxxx29@gmail.com>
2022-03-25 12:08:51 +0900

    rails g controller histories new index edit show

---
+++ app/controllers/histories_controller.rb
@@ -0,0 +1,13 @@
+class HistoriesController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
---
+++ test/controllers/histories_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class HistoriesControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get histories_new_url
+    assert_response :success
+  end
+
+  test "should get index" do
+    get histories_index_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get histories_edit_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get histories_show_url
+    assert_response :success
+  end
+end
