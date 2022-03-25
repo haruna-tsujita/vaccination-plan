38495360f3c4958350cbcdf238fd5a2e5fbfe7c5
napple29 <snwxxx29@gmail.com>
2022-03-25 17:32:00 +0900

    予防接種履歴の日付を保存できるよう変更

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,5 +1,6 @@
 class HistoriesController < ApplicationController
   def new
+    @child = Child.find(params[:child_id])
     @history = History.new
   end
 
@@ -10,5 +11,27 @@ class HistoriesController < ApplicationController
   end
 
   def show
+  end
+
+  def create
+    @child = Child.find(params[:child_id])
+    @history = History.new(history_params)
+    @history.child_id = @child.id
+
+    respond_to do |format|
+      if @history.save
+        format.html { redirect_to history_url(@history), notice: "History was successfully created." }
+        format.json { render :show, status: :created, location: @history }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @history.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def history_params
+    params.require(:history).permit(:date, :vaccinated)
   end
 end
