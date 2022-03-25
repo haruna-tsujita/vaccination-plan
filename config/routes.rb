888c9082e400eaf8b9e70fb32488c3de395df79c
napple29 <snwxxx29@gmail.com>
2022-03-25 12:53:10 +0900

    ルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'histories/new'
-  get 'histories/index'
-  get 'histories/edit'
-  get 'histories/show'
   devise_for :users, path: ''
-  resources :children
+  resources :children do
+    resources :histories
+  end
   root 'top#index'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
