09c7a7584807b5559c8c01b8888980f0abb19e26
napple29 <snwxxx29@gmail.com>
2022-03-27 16:34:24 +0900

    seedに予防接種のデータを入力

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,34 @@
+# coding: utf-8
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+Vaccination.create(:name => JpVaccination.find('hib_1').name ,:period => JpVaccination.find('hib_1').period, :key =>'hib_1')
+Vaccination.create(:name => JpVaccination.find('hib_2').name ,:period => JpVaccination.find('hib_2').period, :key =>'hib_2')
+Vaccination.create(:name => JpVaccination.find('hib_3').name ,:period => JpVaccination.find('hib_3').period, :key =>'hib_3')
+Vaccination.create(:name => JpVaccination.find('hib_4').name ,:period => JpVaccination.find('hib_4').period, :key =>'hib_4')
+Vaccination.create(:name => JpVaccination.find('hepatitis_B_1').name ,:period => JpVaccination.find('hepatitis_B_1').period, :key =>'hepatitis_B_1')
+Vaccination.create(:name => JpVaccination.find('hepatitis_B_2').name ,:period => JpVaccination.find('hepatitis_B_2').period, :key =>'hepatitis_B_2')
+Vaccination.create(:name => JpVaccination.find('hepatitis_B_3').name ,:period => JpVaccination.find('hepatitis_B_3').period, :key =>'hepatitis_B_3')
+Vaccination.create(:name => JpVaccination.find('rotavirus_1').name ,:period => JpVaccination.find('rotavirus_1').period, :key =>'rotavirus_1')
+Vaccination.create(:name => JpVaccination.find('rotavirus_2').name ,:period => JpVaccination.find('rotavirus_2').period, :key =>'rotavirus_2')
+Vaccination.create(:name => JpVaccination.find('rotavirus_3').name ,:period => JpVaccination.find('rotavirus_3').period, :key =>'rotavirus_3')
+Vaccination.create(:name => JpVaccination.find('pneumococcus_1').name ,:period => JpVaccination.find('pneumococcus_1').period, :key =>'pneumococcus_1')
+Vaccination.create(:name => JpVaccination.find('pneumococcus_2').name ,:period => JpVaccination.find('pneumococcus_2').period, :key =>'pneumococcus_2')
+Vaccination.create(:name => JpVaccination.find('pneumococcus_3').name ,:period => JpVaccination.find('pneumococcus_3').period, :key =>'pneumococcus_3')
+Vaccination.create(:name => JpVaccination.find('pneumococcus_4').name ,:period => JpVaccination.find('pneumococcus_4').period, :key =>'pneumococcus_4')
+Vaccination.create(:name => JpVaccination.find('DPT_IPV_1').name ,:period => JpVaccination.find('DPT_IPV_1').period, :key =>'DPT_IPV_1')
+Vaccination.create(:name => JpVaccination.find('DPT_IPV_2').name ,:period => JpVaccination.find('DPT_IPV_2').period, :key =>'DPT_IPV_2')
+Vaccination.create(:name => JpVaccination.find('DPT_IPV_3').name ,:period => JpVaccination.find('DPT_IPV_3').period, :key =>'DPT_IPV_3')
+Vaccination.create(:name => JpVaccination.find('DPT_IPV_4').name ,:period => JpVaccination.find('DPT_IPV_4').period, :key =>'DPT_IPV_4')
+Vaccination.create(:name => JpVaccination.find('DT_1').name ,:period => JpVaccination.find('DT_1').period, :key =>'DT_1')
+Vaccination.create(:name => JpVaccination.find('BCG_1').name ,:period => JpVaccination.find('BCG_1').period, :key =>'BCG_1')
+Vaccination.create(:name => JpVaccination.find('MR_1').name ,:period => JpVaccination.find('MR_1').period, :key =>'MR_1')
+Vaccination.create(:name => JpVaccination.find('MR_2').name ,:period => JpVaccination.find('MR_2').period, :key =>'MR_2')
+Vaccination.create(:name => JpVaccination.find('chickenpox_1').name ,:period => JpVaccination.find('chickenpox_1').period, :key =>'chickenpox_1')
+Vaccination.create(:name => JpVaccination.find('chickenpox_2').name ,:period => JpVaccination.find('chickenpox_2').period, :key =>'chickenpox_2')
+Vaccination.create(:name => JpVaccination.find('mumps_1').name ,:period => JpVaccination.find('mumps_1').period, :key =>'mumps_1')
+Vaccination.create(:name => JpVaccination.find('mumps_2').name ,:period => JpVaccination.find('mumps_2').period, :key =>'mumps_2')
+
+Vaccination.create(:name => JpVaccination.find('Japanese_encephalitis_1').name ,:period => JpVaccination.find('Japanese_encephalitis_1').period, :key =>'Japanese_encephalitis_1')
+Vaccination.create(:name => JpVaccination.find('Japanese_encephalitis_2').name ,:period => JpVaccination.find('Japanese_encephalitis_2').period, :key =>'Japanese_encephalitis_2')
+Vaccination.create(:name => JpVaccination.find('Japanese_encephalitis_3').name ,:period => JpVaccination.find('Japanese_encephalitis_3').period, :key =>'Japanese_encephalitis_3')
+Vaccination.create(:name => JpVaccination.find('Japanese_encephalitis_4').name ,:period => JpVaccination.find('Japanese_encephalitis_4').period, :key =>'Japanese_encephalitis_4')
