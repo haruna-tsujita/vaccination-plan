e6a0e7984018fb4b9da0eaee7bbc61c9520d97b6
napple29 <snwxxx29@gmail.com>
2022-03-29 14:52:01 +0900

    historiesコントローラーを編集

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -6,9 +6,14 @@ class HistoriesController < ApplicationController
     @history = History.new
   end
 
-  def index; end
+  def index
+    @histories = History.where(child_id: params[:child_id])
+  end
 
-  def edit; end
+  def edit
+    @child = Child.find(params[:child_id])
+    @history = History.find(params[:id])
+  end
 
   def show; end
 
@@ -19,7 +24,7 @@ class HistoriesController < ApplicationController
 
     respond_to do |format|
       if @history.save
-        format.html { redirect_to history_url(@history), notice: 'History was successfully created.' }
+        format.html { redirect_to child_histories_url, notice: 'History was successfully created.' }
         format.json { render :show, status: :created, location: @history }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -28,9 +33,24 @@ class HistoriesController < ApplicationController
     end
   end
 
+  def update
+    @child = Child.find(params[:child_id])
+    @history = History.find(params[:id])
+
+    respond_to do |format|
+      if @history.update(history_params)
+        format.html { redirect_to child_histories_url, notice: "History was successfully updated." }
+        format.json { render :show, status: :ok, location: @history }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @history.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def history_params
-    params.require(:history).permit(:date, :vaccinated)
+    params.require(:history).permit(:date, :vaccinated, :vaccination_id, :id)
   end
 end
