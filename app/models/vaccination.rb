9dba92c839f0d87b894aa199af86cd0d97320981
napple29 <snwxxx29@gmail.com>
2022-03-30 11:43:45 +0900

    History#indexの共通部分が重複していたためリファクタリング

--- app/models/vaccination.rb
+++ app/models/vaccination.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Vaccination < ApplicationRecord
+  VACCINATION_NAME = ['ヒブ', 'Ｂ型肝炎', 'ロタウイルス', '小児用肺炎球菌', '４種混合', '２種混合', 'ＢＣＧ', '麻しん・風しん混合', '水痘', 'おたふくかぜ', '日本脳炎'].freeze
+
   has_one :history, dependent: :nullify
 end
