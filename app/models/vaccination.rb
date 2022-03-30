497a59eef601ac868107a4cf15372fb7174712af
napple29 <snwxxx29@gmail.com>
2022-03-30 11:58:09 +0900

    接種履歴の編集画面でなんのワクチンに関しての入力なのかがわかりづらかったためHistory#editにワクチン名を表示させた

--- app/models/vaccination.rb
+++ app/models/vaccination.rb
@@ -4,4 +4,9 @@ class Vaccination < ApplicationRecord
   VACCINATION_NAME = ['ヒブ', 'Ｂ型肝炎', 'ロタウイルス', '小児用肺炎球菌', '４種混合', '２種混合', 'ＢＣＧ', '麻しん・風しん混合', '水痘', 'おたふくかぜ', '日本脳炎'].freeze
 
   has_one :history, dependent: :nullify
+
+  def self.vaccination_formal_name(history)
+    vaccination = Vaccination.find(history.vaccination_id)
+    "#{vaccination.name} #{vaccination.period}"
+  end
 end
