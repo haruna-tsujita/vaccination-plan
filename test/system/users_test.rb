54efc789aae54032c60872cd67427bd9a2c02280
napple29 <snwxxx29@gmail.com>
2022-03-31 10:26:25 +0900

    fixtureで変数名を変更したため、それに伴いテストを一部修正

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -6,9 +6,6 @@ require 'application_system_test_case'
 class Userstest < ApplicationSystemTestCase
   test 'send_confirmation_mail' do
     visit new_user_registration_path
-    # fill_in 'user[name]', with: 'test@example.com'
-    # fill_in 'user[password]', with: 'testtest'
-    # fill_in 'user[password_confirmation]', with: 'testtest'
     fill_in 'user[email]', with: 'test@example.com'
     fill_in 'user[password]', with: 'testtest'
     fill_in 'user[password_confirmation]', with: 'testtest'
