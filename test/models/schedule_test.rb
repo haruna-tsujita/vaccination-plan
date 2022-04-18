fbfe3ecbf0ed152e4acbd1907ef90fd0d4c4d7b5
napple29 <snwxxx29@gmail.com>
2022-04-18 17:11:29 +0900

    nameとperiodの間に半角スペースを追加したことによりテストが落ちるため修正

--- test/models/schedule_test.rb
+++ test/models/schedule_test.rb
@@ -6,8 +6,8 @@ class ScheduleTest < ActiveSupport::TestCase
   test 'future_plans' do
     carol = children(:carol)
     histories = History.where(child: carol)
-    expexted = { Date.parse('2022-03-03') => %w[ヒブ第1期 ロタウイルス1回目], Date.parse('2022-04-03') => %w[ヒブ第2期 ロタウイルス2回目],
-                 Date.parse('2022-05-03') => %w[ヒブ第3期 ロタウイルス3回目], Date.parse('2023-01-03') => ['ヒブ第4期'] }
+    expexted = { Date.parse('2022-03-03') => ['ヒブ 第1期', 'ロタウイルス 1回目'], Date.parse('2022-04-03') => ['ヒブ 第2期', 'ロタウイルス 2回目'],
+                 Date.parse('2022-05-03') => ['ヒブ 第3期', 'ロタウイルス 3回目'], Date.parse('2023-01-03') => ['ヒブ 第4期'] }
     assert_equal expexted, Schedule.future_plans(histories, carol)
   end
 end
