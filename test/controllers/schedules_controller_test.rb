a545405983687639ca505bb89e01fd17db0fc5c3
napple29 <snwxxx29@gmail.com>
2022-04-18 12:30:20 +0900

    rubocopの指摘を修正

--- test/controllers/schedules_controller_test.rb
+++ test/controllers/schedules_controller_test.rb
@@ -1,7 +1,9 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class SchedulesControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get schedules_index_url
     assert_response :success
   end
