81f3c45fd9c509e3432973d733094f7a637bdb9c
napple29 <snwxxx29@gmail.com>
2022-04-18 17:56:11 +0900

    TimeZoneをTokyoに変更

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,8 @@ module VaccinationPlan
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
