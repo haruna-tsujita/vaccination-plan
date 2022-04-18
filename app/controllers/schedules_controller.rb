81e6c53b4df869ea90755e27429f12d8c80cb4b0
napple29 <snwxxx29@gmail.com>
2022-04-18 11:36:32 +0900

    コントローラーに使用する情報を記述

--- app/controllers/schedules_controller.rb
+++ app/controllers/schedules_controller.rb
@@ -1,4 +1,7 @@
 class SchedulesController < ApplicationController
   def index
+    @user = current_user
+    @child = Child.find(params[:child_id])
+    @histories = History.where(child_id: @child.id).order(vaccination_id: :asc)
   end
 end
