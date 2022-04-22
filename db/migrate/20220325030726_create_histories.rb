8bbdf1d4d0859a9dc225314dc199653aed21540c
napple29 <snwxxx29@gmail.com>
2022-04-22 13:01:00 +0900

    カラムの制約に不足があったため修正

--- db/migrate/20220325030726_create_histories.rb
+++ db/migrate/20220325030726_create_histories.rb
@@ -1,11 +1,12 @@
 class CreateHistories < ActiveRecord::Migration[6.1]
   def change
     create_table :histories do |t|
-      t.references :vaccination, foreign_key: true, unique: true
-      t.belongs_to :child, index: true, foreign_key: true
+      t.references :vaccination, foreign_key: true, index: false, null: false
+      t.belongs_to :child, foreign_key: true, index: false, null: false
       t.date :date
       t.boolean :vaccinated
       t.timestamps
     end
+    add_index :histories, [:child_id, :vaccination_id], unique: true
   end
 end
