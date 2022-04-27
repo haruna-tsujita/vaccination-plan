3c6e383e88a2f300ff2801bd1f5f85457a73583e
napple29 <snwxxx29@gmail.com>
2022-04-27 11:37:15 +0900

    テストを修正

--- test/controllers/families_controller_test.rb
+++ test/controllers/families_controller_test.rb
@@ -3,8 +3,4 @@
 require 'test_helper'
 
 class FamiliesControllerTest < ActionDispatch::IntegrationTest
-  test 'should get index' do
-    get families_index_url
-    assert_response :success
-  end
 end
