5b59dca43ca7786e36a654dbea217c4734bf6d0c
napple29 <snwxxx29@gmail.com>
2022-04-29 16:58:17 +0900

    ログイン中にtopページに遷移できないようにした

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class TopController < ApplicationController
-  def index; end
+  def index
+    if current_user.children.size == 0
+      redirect_to new_child_path
+    elsif current_user.children.size == 1
+      redirect_to child_histories_path(current_user.children[0])
+    elsif current_user.children.size > 1
+      redirect_to families_path
+    end
+  end
 end
