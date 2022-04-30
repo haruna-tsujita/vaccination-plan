73877b9eb24b7f3fa05ee65092c3a32a37325989
napple29 <snwxxx29@gmail.com>
2022-04-30 15:23:08 +0900

    コントローラーテストを削除

--- test/controllers/settings_controller_test.rb
+++ test/controllers/settings_controller_test.rb
@@ -1,8 +1,10 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class SettingsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get settings_index_url
-    assert_response :success
-  end
+  # test "should get index" do
+  #   get settings_index_url
+  #   assert_response :success
+  # end
 end
