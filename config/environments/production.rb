305fb113525ffcb5c51455fb37fdc79a8fd0e086
napple29 <snwxxx29@gmail.com>
2022-05-05 02:25:25 +0900

    メールの設定を変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,7 +124,7 @@ Rails.application.configure do
     :address        => ENV['MAILGUN_SMTP_SERVER'],
     :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
     :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => 'yourapp.heroku.com',
+    :domain         => 'vaccination-plan.heroku.com',
     :authentication => :plain,
   }
 end
