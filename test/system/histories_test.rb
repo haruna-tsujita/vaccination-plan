e9ba625c0199b88b232d51a4c861756c4406c750
napple29 <snwxxx29@gmail.com>
2022-05-08 10:38:02 +0900

    viewの文言を変更したため、テストを修正

--- test/system/histories_test.rb
+++ test/system/histories_test.rb
@@ -22,7 +22,7 @@ class Historiestest < ApplicationSystemTestCase
     setup_alice
     alice_child = children(:carol)
     visit edit_child_history_path(alice_child.id, histories(:carol_history_hib_first).id)
-    fill_in '接種した日', with: Date.parse('2022-02-01')
+    fill_in '接種日', with: Date.parse('2022-02-01')
     click_on '登録する'
     assert_text '接種日時が保存されました'
     assert_text '22/02/01'
@@ -47,7 +47,7 @@ class Historiestest < ApplicationSystemTestCase
     setup_alice
     carol = children(:carol)
     visit edit_child_history_path(carol.id, histories(:carol_history_hib_first).id)
-    fill_in '接種した日', with: Date.current
+    fill_in '接種日', with: Date.current
     click_on '登録する'
     assert_text Date.current.strftime('%y/%m/%d')
   end
@@ -56,7 +56,7 @@ class Historiestest < ApplicationSystemTestCase
     setup_alice
     carol = children(:carol)
     visit edit_child_history_path(carol.id, histories(:carol_history_hib_first).id)
-    fill_in '接種した日', with: Date.current + 1.day
+    fill_in '接種日', with: Date.current + 1.day
     click_on '登録する'
     assert_text '接種日時は今日より前の日付にしてください'
   end
@@ -66,7 +66,7 @@ class Historiestest < ApplicationSystemTestCase
     carol = children(:carol)
     histories(:carol_history_rotavirus_second).update(date: Date.current - 1.month)
     visit edit_child_history_path(carol.id, histories(:carol_history_rotavirus_third).id)
-    fill_in '接種した日', with: Date.current - 1.month - 1.day
+    fill_in '接種日', with: Date.current - 1.month - 1.day
     click_on '登録する'
     assert_text '接種日時が前回の期より前の日付になっています'
   end
@@ -76,7 +76,7 @@ class Historiestest < ApplicationSystemTestCase
     carol = children(:carol)
     histories(:carol_history_rotavirus_second).update(date: Date.current - 1.month)
     visit edit_child_history_path(carol.id, histories(:carol_history_rotavirus_third).id)
-    fill_in '接種した日', with: Date.current - 1.month + 1.day
+    fill_in '接種日', with: Date.current - 1.month + 1.day
     click_on '登録する'
     assert_text '接種日時が保存されました'
   end
@@ -86,7 +86,7 @@ class Historiestest < ApplicationSystemTestCase
     carol = children(:carol)
     histories(:carol_history_rotavirus_third).update(date: Date.current - 1.month)
     visit edit_child_history_path(carol.id, histories(:carol_history_rotavirus_second).id)
-    fill_in '接種した日', with: Date.current - 1.month + 1.day
+    fill_in '接種日', with: Date.current - 1.month + 1.day
     click_on '登録する'
     assert_text '接種日時が次回の期より後の日付になっています'
   end
@@ -96,7 +96,7 @@ class Historiestest < ApplicationSystemTestCase
     carol = children(:carol)
     histories(:carol_history_rotavirus_third).update(date: Date.current - 1.month)
     visit edit_child_history_path(carol.id, histories(:carol_history_rotavirus_second).id)
-    fill_in '接種した日', with: Date.current - 1.month - 1.day
+    fill_in '接種日', with: Date.current - 1.month - 1.day
     click_on '登録する'
     assert_text '接種日時が保存されました'
   end
@@ -106,7 +106,7 @@ class Historiestest < ApplicationSystemTestCase
     dave = children(:dave)
     visit "/children/#{dave.id}/histories/#{History.find_by(child_id: dave.id, vaccination_id: Vaccination.find_by(key: 'hib_2').id).id}/edit"
     assert_text 'ヒブ 第2期'
-    fill_in '接種した日', with: Date.current
+    fill_in '接種日', with: Date.current
     click_on '登録する'
     assert_equal true, History.find_by(child_id: dave.id, vaccination_id: Vaccination.find_by(key: 'hib_1').id).vaccinated
     assert_nil History.find_by(child_id: dave.id, vaccination_id: Vaccination.find_by(key: 'hib_3').id).vaccinated
