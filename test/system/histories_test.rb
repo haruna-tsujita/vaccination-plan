50f8411ff4c22fbf7787d24b9cc616802d0e4dcb
napple29 <snwxxx29@gmail.com>
2022-05-10 00:44:05 +0900

    システムテストを修正

--- test/system/histories_test.rb
+++ test/system/histories_test.rb
@@ -21,41 +21,40 @@ class Historiestest < ApplicationSystemTestCase
   test 'edit history when user logged in' do
     setup_alice
     alice_child = children(:carol)
-    visit edit_child_history_path(alice_child.id, histories(:carol_history_hib_first).id)
+    visit edit_child_history_path(alice_child.id, histories(:carol_history_hib_first).id, vaccination_id: vaccinations(:hib_1))
     fill_in '接種日', with: Date.parse('2022-02-01')
     click_on '登録する'
     assert_text '接種日時が保存されました'
-    assert_text '22/02/01'
     assert_text '❶'
   end
 
   test 'not edit history before user logged in' do
     setup_alice
     logout
-    visit edit_child_history_path(children(:carol).id, histories(:carol_history_hib_first).id)
+    visit edit_child_history_path(children(:carol).id, histories(:carol_history_hib_first).id, vaccination_id: vaccinations(:hib_1))
     assert_text 'ログインもしくはアカウント登録してください。'
   end
 
   test 'not edit any child other than their own' do
     setup_alice
     bob_child = children(:dave)
-    visit edit_child_history_path(bob_child.id, histories(:dave_history_hib_first).id)
+    visit edit_child_history_path(bob_child.id, histories(:dave_history_hib_first).id, vaccination_id: vaccinations(:hib_1))
     assert_text '子どもの登録'
   end
 
   test 'validation history before today' do
     setup_alice
     carol = children(:carol)
-    visit edit_child_history_path(carol.id, histories(:carol_history_hib_first).id)
+    visit edit_child_history_path(carol.id, histories(:carol_history_hib_first).id, vaccination_id: vaccinations(:hib_1))
     fill_in '接種日', with: Date.current
     click_on '登録する'
-    assert_text Date.current.strftime('%y/%m/%d')
+    assert_text'接種日時が保存されました'
   end
 
   test 'validation not update history after tommorow' do
     setup_alice
     carol = children(:carol)
-    visit edit_child_history_path(carol.id, histories(:carol_history_hib_first).id)
+    visit edit_child_history_path(carol.id, histories(:carol_history_hib_first).id, vaccination_id: vaccinations(:hib_1))
     fill_in '接種日', with: Date.current + 1.day
     click_on '登録する'
     assert_text '接種日時は今日より前の日付にしてください'
@@ -65,7 +64,7 @@ class Historiestest < ApplicationSystemTestCase
     setup_alice
     carol = children(:carol)
     histories(:carol_history_rotavirus_second).update(date: Date.current - 1.month)
-    visit edit_child_history_path(carol.id, histories(:carol_history_rotavirus_third).id)
+    visit edit_child_history_path(carol.id, histories(:carol_history_rotavirus_third).id, vaccination_id: vaccinations(:rotavirus_3))
     fill_in '接種日', with: Date.current - 1.month - 1.day
     click_on '登録する'
     assert_text '接種日時が前回の期より前の日付になっています'
@@ -75,7 +74,7 @@ class Historiestest < ApplicationSystemTestCase
     setup_alice
     carol = children(:carol)
     histories(:carol_history_rotavirus_first).update(date: Date.current - 2.months)
-    visit edit_child_history_path(carol.id, histories(:carol_history_rotavirus_third).id)
+    visit edit_child_history_path(carol.id, histories(:carol_history_rotavirus_third).id, vaccination_id: vaccinations(:rotavirus_3))
     fill_in '接種日', with: Date.current - 2.months - 1.day
     click_on '登録する'
     assert_text '接種日時が前回の期より前の日付になっています'
@@ -85,7 +84,7 @@ class Historiestest < ApplicationSystemTestCase
     setup_alice
     carol = children(:carol)
     histories(:carol_history_rotavirus_second).update(date: Date.current - 1.month)
-    visit edit_child_history_path(carol.id, histories(:carol_history_rotavirus_third).id)
+    visit edit_child_history_path(carol.id, histories(:carol_history_rotavirus_third).id, vaccination_id: vaccinations(:rotavirus_3))
     fill_in '接種日', with: Date.current - 1.month + 1.day
     click_on '登録する'
     assert_text '接種日時が保存されました'
@@ -95,7 +94,7 @@ class Historiestest < ApplicationSystemTestCase
     setup_alice
     carol = children(:carol)
     histories(:carol_history_rotavirus_third).update(date: Date.current - 1.month)
-    visit edit_child_history_path(carol.id, histories(:carol_history_rotavirus_second).id)
+    visit edit_child_history_path(carol.id, histories(:carol_history_rotavirus_second).id, vaccination_id: vaccinations(:rotavirus_2))
     fill_in '接種日', with: Date.current - 1.month + 1.day
     click_on '登録する'
     assert_text '接種日時が次回の期より後の日付になっています'
@@ -105,7 +104,7 @@ class Historiestest < ApplicationSystemTestCase
     setup_alice
     carol = children(:carol)
     histories(:carol_history_hib_fourth).update(date: Date.current - 1.month)
-    visit edit_child_history_path(carol.id, histories(:carol_history_hib_first).id)
+    visit edit_child_history_path(carol.id, histories(:carol_history_hib_first).id, vaccination_id: vaccinations(:hib_1))
     fill_in '接種日', with: Date.current - 1.month + 1.day
     click_on '登録する'
     assert_text '接種日時が次回の期より後の日付になっています'
@@ -115,21 +114,9 @@ class Historiestest < ApplicationSystemTestCase
     setup_alice
     carol = children(:carol)
     histories(:carol_history_rotavirus_third).update(date: Date.current - 1.month)
-    visit edit_child_history_path(carol.id, histories(:carol_history_rotavirus_second).id)
+    visit edit_child_history_path(carol.id, histories(:carol_history_rotavirus_second).id, vaccination_id: vaccinations(:rotavirus_2))
     fill_in '接種日', with: Date.current - 1.month - 1.day
     click_on '登録する'
     assert_text '接種日時が保存されました'
-  end
-
-  test 'automatically_vaccinated' do
-    setup_bob
-    dave = children(:dave)
-    visit "/children/#{dave.id}/histories/#{History.find_by(child_id: dave.id, vaccination_id: Vaccination.find_by(key: 'hib_2').id).id}/edit"
-    assert_text 'ヒブ 第2期'
-    fill_in '接種日', with: Date.current
-    click_on '登録する'
-    assert_equal true, History.find_by(child_id: dave.id, vaccination_id: Vaccination.find_by(key: 'hib_1').id).vaccinated
-    assert_nil History.find_by(child_id: dave.id, vaccination_id: Vaccination.find_by(key: 'hib_3').id).vaccinated
-    assert_nil History.find_by(child_id: dave.id, vaccination_id: Vaccination.find_by(key: 'hib_4').id).vaccinated
   end
 end
