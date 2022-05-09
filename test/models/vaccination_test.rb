68eb9e52f50ae9b48cb154f3d2a458db0a761c59
napple29 <snwxxx29@gmail.com>
2022-05-09 15:42:32 +0900

    Vaccinationクラスのモデルテストを追加

--- test/models/vaccination_test.rb
+++ test/models/vaccination_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class VaccinationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'when regular vaccination' do
+    regular_vaccination = vaccinations(:pneumococcus_2).name
+    assert Vaccination.regular?(regular_vaccination)
+  end
+
+  test 'when not regular vaccination' do
+    not_regular_vaccination = vaccinations(:mumps_2).name
+    assert_not Vaccination.regular?(not_regular_vaccination)
+  end
+
+  test 'output formal_name' do
+    history = histories(:dave_history_chickenpox_second)
+    assert_equal '水痘 2回目', Vaccination.vaccination_formal_name(history)
+  end
 end
