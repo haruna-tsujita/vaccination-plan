1c62fb216717b5fedcf805d7a15b6c378d0d6dec
napple29 <snwxxx29@gmail.com>
2022-05-10 00:42:26 +0900

    モデルテストを作成

---
+++ test/models/history_test.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class HistoryTest < ActiveSupport::TestCase
+
+  test 'automatically_vaccinated' do
+    carol = children(:carol)
+    vaccination = vaccinations(:pneumococcus_3)
+    history = History.new(child: carol, vaccination: vaccination, date: Date.current)
+    history.save
+    History.automatically_vaccinated(vaccination.id, carol.id)
+    assert_equal true, History.find_by(child: carol, vaccination: Vaccination.find_by(key: vaccinations(:'pneumococcus_1').key)).vaccinated
+    assert_equal true, History.find_by(child: carol, vaccination: Vaccination.find_by(key: vaccinations(:'pneumococcus_2').key)).vaccinated
+    assert_nil History.find_by(child: carol, vaccination: Vaccination.find_by(key: vaccinations(:'pneumococcus_4').key))
+  end
+
+end
