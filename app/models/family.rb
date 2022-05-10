c86fe7384ace6dc4821b92f3b0e8d1293c6e4480
napple29 <snwxxx29@gmail.com>
2022-05-10 17:40:28 +0900

    メソッドを見直したため引数などを修正

--- app/models/family.rb
+++ app/models/family.rb
@@ -1,26 +1,25 @@
 # frozen_string_literal: true
 
 class Family
-  def self.family_schedule(histories)
-    all_histories = histories.map do |child_histories|
-      child = child_histories[0].child
-      Schedule.future_plans(child_histories, child)
+  def self.family_schedule(vaccinations, children)
+    all_histories = children.map do |child|
+      Schedule.future_plans(vaccinations, child)
     end
 
     merge_history = {}.merge(*all_histories) { |_key, histories1, histories2| histories1 + histories2 }
     sort_plan = merge_history.sort_by do |date, _vaccination|
       [Date, String].include?(date.class) ? date : date.first
     end.to_h
-    sort_plan.each_value do |vaccinations|
-      vaccinations.sort_by! do |vaccination|
-        vaccination[:child]
-        vaccination[:name]
+    sort_plan.each_value do |vacs|
+      vacs.sort_by! do |vac|
+        vac[:child]
+        vac[:name]
       end
     end.to_h
   end
 
-  def self.vaccination_date_before_today(histories)
-    Family.family_schedule(histories).select do |date, vaccination|
+  def self.vaccination_date_before_today(histories, children)
+    Family.family_schedule(histories, children).select do |date, vaccination|
       { date => vaccination } if (date.instance_of?(Range) && date.first <= Time.current) || (date.instance_of?(Date) && date <= Time.current)
     end
   end
