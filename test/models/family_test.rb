339cc80e22442d40cdb6e57f8f75c8f1425b595a
napple29 <snwxxx29@gmail.com>
2022-05-10 21:32:32 +0900

    モデルテストを追加

--- test/models/family_test.rb
+++ test/models/family_test.rb
@@ -54,21 +54,27 @@ class FamilyTest < ActiveSupport::TestCase
     frank = children(:frank)
     children = [dave, frank]
     vaccinations = Vaccination.all.order(:id)
-    # expected = {
-    #   Date.parse('2019-11-10') => [{ name: 'ヒブ', period: '第1期', child: frank }, { name: 'ロタウイルス', period: '1回目', child: frank },
-    #                                { name: '小児用肺炎球菌', period: '1回目', child: frank }],
-    #   Date.parse('2019-12-10') => [{ name: 'ヒブ', period: '第2期', child: frank }, { name: 'ロタウイルス', period: '2回目', child: frank },
-    #                                { name: '小児用肺炎球菌', period: '2回目', child: frank }],
-    #   Date.parse('2020-01-10') => [{ name: 'ヒブ', period: '第3期', child: frank }, { name: 'ロタウイルス', period: '3回目', child: frank },
-    #                                { name: '小児用肺炎球菌', period: '3回目', child: frank }],
-    #   Date.parse('2020-09-10') => [{ name: 'ヒブ', period: '第4期', child: frank }, { name: '小児用肺炎球菌', period: '4回目', child: frank },
-    #                                { name: '水痘', period: '1回目', child: frank }],
-    #   Date.parse('2021-03-10') => [{ name: '水痘', period: '2回目', child: frank }],
-    #   Date.parse('2021-09-21') => [{ name: 'ヒブ', period: '第1期', child: dave }, { name: '小児用肺炎球菌', period: '1回目', child: dave }],
-    #   Date.parse('2021-10-21') => [{ name: 'ヒブ', period: '第2期', child: dave }, { name: '小児用肺炎球菌', period: '2回目', child: dave }],
-    #   Date.parse('2021-11-18') => [{ name: 'ロタウイルス', period: '3回目', child: dave }],
-    #   Date.parse('2021-11-21') => [{ name: 'ヒブ', period: '第3期', child: dave }, { name: '小児用肺炎球菌', period: '3回目', child: dave }]
-    # }
+
+    expected = { Date.parse('2019-11-10') => [{ name: 'ヒブ', period: '第1期', child: frank }, { name: 'ロタウイルス', period: '1回目', child: frank }, { name: '小児用肺炎球菌', period: '1回目', child: frank }, { name: 'Ｂ型肝炎', period: '1回目', child: frank }],
+                 Date.parse('2019-12-10') => [{ name: 'ヒブ', period: '第2期', child: frank }, { name: 'ロタウイルス', period: '2回目', child: frank }, { name: '小児用肺炎球菌', period: '2回目', child: frank },
+                                              { name: '４種混合', period: '1回目', child: frank }, { name: 'Ｂ型肝炎', period: '2回目', child: frank }],
+                 Date.parse('2020-01-10') => [{ name: 'ヒブ', period: '第3期', child: frank }, { name: 'ロタウイルス', period: '3回目', child: frank }, { name: '小児用肺炎球菌', period: '3回目', child: frank },
+                                              { name: '４種混合', period: '2回目', child: frank }],
+                 Date.parse('2020-02-10') => [{ name: '４種混合', period: '3回目', child: frank }, { name: 'ＢＣＧ', period: 'nil', child: frank }],
+                 Date.parse('2020-04-10') => [{ name: 'Ｂ型肝炎', period: '3回目', child: frank }],
+                 Date.parse('2020-09-10') => [{ name: 'おたふくかぜ', period: '1回目', child: frank }, { name: 'ヒブ', period: '第4期', child: frank }, { name: '小児用肺炎球菌', period: '4回目', child: frank },
+                                              { name: '水痘', period: '1回目', child: frank }, { name: '麻しん・風しん混合', period: '第1期', child: frank }],
+                 Date.parse('2021-02-10') => [{ name: '４種混合', period: '4回目', child: frank }],
+                 Date.parse('2021-03-10') => [{ name: '水痘', period: '2回目', child: frank }],
+                 Date.parse('2021-09-21') => [{ name: 'ヒブ', period: '第1期', child: dave }, { name: '小児用肺炎球菌', period: '1回目', child: dave }, { name: 'Ｂ型肝炎', period: '1回目', child: dave }],
+                 Date.parse('2021-10-21') => [{ name: 'ヒブ', period: '第2期', child: dave }, { name: '小児用肺炎球菌', period: '2回目', child: dave },
+                                              { name: '４種混合', period: '1回目', child: dave }, { name: 'Ｂ型肝炎', period: '2回目', child: dave }],
+                 Date.parse('2021-11-18') => [{ name: 'ロタウイルス', period: '3回目', child: dave }],
+                 Date.parse('2021-11-21') => [{ name: 'ヒブ', period: '第3期', child: dave }, { name: '小児用肺炎球菌', period: '3回目', child: dave },
+                                              { name: '４種混合', period: '2回目', child: dave }],
+                 Date.parse('2021-12-21') => [{ name: '４種混合', period: '3回目', child: dave }, { name: 'ＢＣＧ', period: 'nil', child: dave }],
+                 Date.parse('2022-02-21') => [{ name: 'Ｂ型肝炎', period: '3回目', child: dave }],
+                 }
 
     assert_equal expected, Family.vaccination_date_before_today(vaccinations, children)
   end
