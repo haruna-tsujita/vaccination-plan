a747dde45021120d1f7dc68fac27ae1e1955aa3a
napple29 <snwxxx29@gmail.com>
2022-05-13 17:31:41 +0900

    developmentの設定を元に戻した

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,17 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  # config.action_mailer.delivery_method = :letter_opener_web
-  config.action_mailer.default_url_options = {  host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:"smtp.gmail.com",
-    domain: 'gmail.com',
-    port:587,
-    user_name: Rails.application.credentials.gmail.user_name,
-    password: Rails.application.credentials.gmail.password,
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :letter_opener_web
 end
