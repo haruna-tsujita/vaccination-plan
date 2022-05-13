358b3e4ea0cf189f1292daa83f869949fad13477
napple29 <snwxxx29@gmail.com>
2022-05-13 14:39:06 +0900

    使っていない定数を削除

--- app/models/vaccination.rb
+++ app/models/vaccination.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Vaccination < ApplicationRecord
-  VACCINATION_NAME = ['ヒブ', 'Ｂ型肝炎', 'ロタウイルス', '小児用肺炎球菌', '４種混合', '２種混合', 'ＢＣＧ', '麻しん・風しん混合', '水痘', 'おたふくかぜ', '日本脳炎'].freeze
-
   has_one :history, dependent: :nullify
 
   def self.regular?(vaccination_name)
