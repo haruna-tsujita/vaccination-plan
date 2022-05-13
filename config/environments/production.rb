cbdb4e0e37de7d96e1ba1a8b37e8ce4565cfd224
napple29 <snwxxx29@gmail.com>
2022-05-13 17:22:03 +0900

    メールの設定をgmailに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,13 +120,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   host = 'vaccination-plan.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    :port           => ENV['MAILGUN_SMTP_PORT'],
-    :address        => ENV['MAILGUN_SMTP_SERVER'],
-    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
-    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-    :domain         => host,
-    :authentication => :plain,
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:"smtp.gmail.com",
+    domain: 'gmail.com',
+    port:587,
+    user_name: Rails.application.credentials.gmail[:user_name],
+    password: Rails.application.credentials.gmail[:password],
+    authentication: 'plain',
+    enable_starttls_auto: true
   }
 end
