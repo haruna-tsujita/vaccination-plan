a346116c915c7bbde4bddd0ea0abfbbf96f3e111
napple29 <snwxxx29@gmail.com>
2022-05-21 15:47:40 +0900

    不要になった記述を削除

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,14 +120,4 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   host = 'vaccination-plan.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:"smtp.gmail.com",
-    domain: 'gmail.com',
-    port:587,
-    user_name: Rails.application.credentials.gmail[:user_name],
-    password: Rails.application.credentials.gmail[:password],
-    authentication: 'plain',
-    enable_starttls_auto: true
-  }
 end
