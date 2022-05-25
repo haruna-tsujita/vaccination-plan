bdfa0d3145cba498c55350b30eee02037c5e4987
napple29 <snwxxx29@gmail.com>
2022-05-25 16:18:21 +0900

    top_controllerと重複している部分を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,16 +4,6 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, if: :except_top_page
   before_action :set_active_storage_host
 
-  def after_sign_in_path_for(resource)
-    if resource.sign_in_count == 1 || current_user.children.size.zero?
-      new_child_path
-    elsif current_user.children.size == 1
-      child_schedules_path(current_user.children[0])
-    elsif current_user.children.size > 1
-      families_path
-    end
-  end
-
   def after_sign_out_path_for(_resource)
     new_user_session_path # ログアウト後に遷移するpathを設定
   end
