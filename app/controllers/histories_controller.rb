2253abbaa22cdd495ae89396b8a685cc317c00e2
napple29 <snwxxx29@gmail.com>
2022-05-25 14:15:10 +0900

    レビューをもとにhistories_controllerを修正

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,82 +1,56 @@
 # frozen_string_literal: true
 
 class HistoriesController < ApplicationController
-  before_action :show_page_own_children_only
-  before_action :set_child_and_vaccination, only: %i[new edit create update]
+  before_action :set_child, only: %i[new edit create update index]
+  before_action :set_vaccination, only: %i[new edit create update]
 
   def new
-    @history = History.new
-    @history.child_id = @child.id
+    @history = @child.histories.new
     @history.vaccination_id = @vaccination.id
   end
 
   def index
-    @child = Child.find(params[:child_id])
-    @vaccinations = Vaccination.all.order(:id)
+    @vaccinations = Vaccination.all.order(:key)
   end
 
   def edit
-    @history = History.find(params[:id])
-    @history.child_id = @child.id
-    @history.vaccination_id = @vaccination.id
+    @history = @child.histories.find(params[:id])
   end
 
   def create
-    @history = History.new
-    @history.child_id = @child.id
+    @history = @child.histories.new
     @history.vaccination_id = @vaccination.id
 
-    respond_to do |format|
-      if @history.update(history_params)
-        History.automatically_vaccinated(@vaccination.id, @child.id)
-        format.html { redirect_to child_histories_url, notice: '接種日時が保存されました' }
-        format.json { render :show, status: :ok, location: @history }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @history.errors, status: :unprocessable_entity }
-      end
+    if @history.update(history_params)
+      History.automatically_vaccinated(@vaccination.id, @child.id)
+      redirect_to child_histories_url, notice: '接種日時が保存されました'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   def update
-    @history = History.find(params[:id])
-    @history.child_id = @child.id
-    @history.vaccination_id = @vaccination.id
+    @history = @child.histories.find(params[:id])
 
-    respond_to do |format|
-      if @history.update(history_params)
-        History.automatically_vaccinated(@history.vaccination_id, @history.child_id)
-        format.html { redirect_to child_histories_url, notice: '接種日時が保存されました' }
-        format.json { render :show, status: :ok, location: @history }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @history.errors, status: :unprocessable_entity }
-      end
+    if @history.update(history_params)
+      History.automatically_vaccinated(@history.vaccination_id, @history.child_id)
+      redirect_to child_histories_url, notice: '接種日時が保存されました'
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
   private
 
   def history_params
-    params.require(:history).permit(:date, :vaccinated, :vaccination_id, :id)
+    params.require(:history).permit(:date, :vaccinated, :vaccination_id)
   end
 
-  def set_child_and_vaccination
-    @child = Child.find(params[:child_id])
-    @vaccination = vaccination_params
+  def set_child
+    @child = current_user.children.find(params[:child_id])
   end
 
-  def show_page_own_children_only
-    return if current_user == Child.find(params[:child_id]).user
-
-    redirect_to new_child_path
-  end
-
-  def vaccination_params
-    if params[:vaccination_id]
-      Vaccination.find(params[:vaccination_id])
-    else
-      history.vaccination_id
-    end
+  def set_vaccination
+    @vaccination = Vaccination.find(params[:vaccination_id])
   end
 end
