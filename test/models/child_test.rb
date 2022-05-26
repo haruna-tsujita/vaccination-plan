bd47d61811997f02f7fc91f7de0abaecd89c2b1f
napple29 <snwxxx29@gmail.com>
2022-05-26 17:38:27 +0900

    上記関連部分を修正

--- test/models/child_test.rb
+++ test/models/child_test.rb
@@ -4,20 +4,20 @@ require 'test_helper'
 
 class ChildTest < ActiveSupport::TestCase
   test 'calc_moon_age birthday is same year' do
-    today = Date.parse('2022-04-22')
-    birthday = Date.parse('2022-01-01')
-    assert_equal '0才 3ヶ月', Child.calc_moon_age(today, birthday)
+    today = Date.parse('2018-12-22')
+    child = children(:issac)
+    assert_equal '0才 1ヶ月', child.calc_moon_age(today)
   end
 
   test 'calc_moon_age birthday is different year' do
-    today = Date.parse('2022-04-22')
-    birthday = Date.parse('2021-12-31')
-    assert_equal '0才 3ヶ月', Child.calc_moon_age(today, birthday)
+    today = Date.parse('2019-04-22')
+    child = children(:issac)
+    assert_equal '0才 5ヶ月', child.calc_moon_age(today)
   end
 
   test 'calc_moon_age birthday is different year and early month' do
-    today = Date.parse('2022-04-22')
-    birthday = Date.parse('2021-01-01')
-    assert_equal '1才 3ヶ月', Child.calc_moon_age(today, birthday)
+    today = Date.parse('2021-04-22')
+    child = children(:eve)
+    assert_equal '1才 1ヶ月', child.calc_moon_age(today)
   end
 end
