29b4e2f71028a6f35f1f6e25ec6d56f1ece966ad
napple29 <snwxxx29@gmail.com>
2022-05-27 13:38:15 +0900

    メソッド呼び出し側を修正

--- test/models/vaccination_test.rb
+++ test/models/vaccination_test.rb
@@ -5,13 +5,13 @@ require 'test_helper'
 class VaccinationTest < ActiveSupport::TestCase
   test 'when regular vaccination' do
     vaccination = 'pneumococcus_2'
-    regular_vaccination = vaccinations(:"#{vaccination}").name # rubocopに指摘されるので
-    assert Vaccination.regular?(regular_vaccination)
+    regular_vaccination = vaccinations(:"#{vaccination}") # rubocopに指摘されるので
+    assert regular_vaccination.regular?
   end
 
   test 'when not regular vaccination' do
     vaccination = 'mumps_2'
-    not_regular_vaccination = vaccinations(:"#{vaccination}").name # rubocopに指摘されるので
-    assert_not Vaccination.regular?(not_regular_vaccination)
+    not_regular_vaccination = vaccinations(:"#{vaccination}") # rubocopに指摘されるので
+    assert_not not_regular_vaccination.regular?
   end
 end
