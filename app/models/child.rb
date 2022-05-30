73b36188e068bcbfe5683886fba0e53f565acf9a
napple29 <snwxxx29@gmail.com>
2022-05-30 22:58:53 +0900

    不正なデータを削除し、avatar_urlをもとに戻した

--- app/models/child.rb
+++ app/models/child.rb
@@ -18,7 +18,7 @@ class Child < ApplicationRecord
 
   def avatar_url
     if avatar.attached?
-      avatar
+      avatar.variant(resize: AVATAR_SIZE).processed.url
     else
       'default.png'
     end
