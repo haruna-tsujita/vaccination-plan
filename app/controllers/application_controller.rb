59515ba9b0dbe79c18f5c70c3b1d7ac86542a661
napple29 <snwxxx29@gmail.com>
2022-07-23 12:22:02 +0900

    画面遷移を一部修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: %i[tos pp]
   before_action :set_active_storage_host
 
   def after_sign_out_path_for(_resource)
-    new_user_session_path # ログアウト後に遷移するpathを設定
+    root_path
   end
 
   private
