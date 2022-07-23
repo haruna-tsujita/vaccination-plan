a2a43306f3f7ddc362056672ce9d2f407965aac9
napple29 <snwxxx29@gmail.com>
2022-07-23 13:52:48 +0900

    RuboCopの指摘を修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: %i[tos pp]
+  before_action :authenticate_user!
   before_action :set_active_storage_host
 
   def after_sign_out_path_for(_resource)
